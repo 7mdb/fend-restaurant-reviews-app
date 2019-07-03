50bde3ddf0d7d194905e8a2a910fdaa965c2747f
Hamad Abaalharith <43918241+7mdb@users.noreply.github.com>
2019-07-03 19:58:45 +0300

    Add files to the repository

---
+++ js/register.js
@@ -0,0 +1,12 @@
+// ServiceWorker registration
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Registration worked!");
+    })
+    .catch(function() {
+      console.log("Registration failed!");
+    });
+}
